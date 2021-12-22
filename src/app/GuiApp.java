c23df6f13df4fbc033100b8b24ffd5d589a8e57d
TeacherIVKHK <teacherIvkhk@ivkhk.ee>
2021-12-22 15:19:23 +0200

    Создали модальное диалоговое окно входа. Надо настраивать

--- src/app/GuiApp.java
+++ src/app/GuiApp.java
@@ -5,6 +5,8 @@
  */
 package app;
 
+import app.mycomopnents.ButtonComponent;
+import app.mycomopnents.EditorComponent;
 import app.mycomopnents.GuestButtonsComponent;
 import app.mycomopnents.GuestComponent;
 import app.mycomopnents.TabAddReaderComponents;
@@ -19,11 +21,13 @@ import facade.ReaderFacade;
 import facade.RoleFacade;
 import facade.UserFacade;
 import facade.UserRolesFacade;
+import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 import javax.swing.BoxLayout;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
@@ -97,6 +101,21 @@ public class GuiApp extends JFrame{
         guestButtonsComponent.getButton1().addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent ae) {
+                JDialog dialogLogin = new JDialog(guiApp,"Введите логи и пароль",Dialog.ModalityType.DOCUMENT_MODAL);
+                dialogLogin.setPreferredSize(new Dimension(GuiApp.WITH_WINDOWS,GuiApp.HEIGHT_WINDOWS));
+                dialogLogin.setMaximumSize(dialogLogin.getPreferredSize());
+                dialogLogin.setMinimumSize(dialogLogin.getPreferredSize());
+                dialogLogin.getContentPane().setLayout(new BoxLayout(dialogLogin.getContentPane(), BoxLayout.Y_AXIS));
+                dialogLogin.setLocationRelativeTo(null);
+                EditorComponent loginComponent = new EditorComponent("Логин", GuiApp.WITH_WINDOWS, 27, 200);
+                EditorComponent passwordComponent = new EditorComponent("Пароль", GuiApp.WITH_WINDOWS, 27, 200);
+                ButtonComponent enterComponent = new ButtonComponent("Войти", GuiApp.WITH_WINDOWS, 27, 200, 150);
+                
+                dialogLogin.getContentPane().add(loginComponent);
+                dialogLogin.getContentPane().add(passwordComponent);
+                dialogLogin.getContentPane().add(enterComponent);
+                dialogLogin.pack();
+                dialogLogin.setVisible(true);
                 
             }
         });
